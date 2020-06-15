1eb6e7fdce1dba859c66fcf4f6d056c8cb3f8e89
Magnopach <magnopacheco67@gmail.com>
2020-06-15 16:47:26 -0300

    .

---
+++ public/scripts/intro.js
@@ -0,0 +1,67 @@
+// comments
+// document.write("hello")
+
+// variaveis, tipos de dados
+// var myvar // undefined
+// myvar = "Hello"
+// const myconst = "Hello"
+
+// document.write(myconst + myvar)
+
+// // string
+// "isso" 
+// 'isso tbm'
+// `ìsso tbm`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     return "run Forest"
+//   }
+// }
+
+// document.write(pessoa.correr())
+
+// // array - vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {}]
+
+// document.write(colecaoDeBolinhas[2])
+
+// funçoes - funcionalidades - atalhos - reuso de codigos
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Magno")
+
+// condicionais
+
+// const notaFinal = 3
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+
+// //  loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+
